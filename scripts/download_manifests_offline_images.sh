644d2e1b729fea7bcf9f9d40af01438d3d5f6b6b
huangzhangshu <huangzhangshu@apecloud.com>
2024-09-26 19:16:57 +0800

    chore: download by ossutil

--- scripts/download_manifests_offline_images.sh
+++ scripts/download_manifests_offline_images.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-MANIFESTS_FILE=${1:-"manifests.yaml"}
-VALUES_FILE=${2:-"values.yaml"}
-PARALLEL_DOWNLOAD=${3:-"true"}
+OSS_ACCESS_KEY_ID=${1:-""}
+OSS_ACCESS_KEY_SECRET=${2:-""}
+MANIFESTS_FILE=${3:-"manifests.yaml"}
+VALUES_FILE=${4:-"values.yaml"}
+PARALLEL_DOWNLOAD=${5:-"true"}
+
 MAX_PARALLEL_NUM=4
 PARALLEL_FILE="download_manifests_offline_images_parallel.txt"
 PACKAGE_DIR="manifests_offline_images_package"
-TOOLS_BASE_URL="http://kubeblocks-oss.oss-cn-zhangjiakou.aliyuncs.com/tools"
-IMAGE_BASE_URL="http://kubeblocks-oss.oss-cn-zhangjiakou.aliyuncs.com/images"
-
+IMAGE_BASE_URL="oss://kubeblocks-oss/images"
+OSS_ENDPOINT="oss-cn-zhangjiakou.aliyuncs.com"
 
 download_images_package() {
     image_package_name=$1
@@ -22,10 +24,11 @@ download_images_package() {
     fi
 
     image_package_url="${IMAGE_BASE_URL}/${image_package_name}-images-${image_package_version}.tar.gz"
+    echo "download image ${image_package_name} ${image_package_version}..."
     for i in {1..3}; do
-        wget -nc -P ${PACKAGE_DIR} ${image_package_url}
-        ret_wget=$?
-        if [[ $ret_msg -eq 0 ]]; then
+        ossutil cp -rf ${image_package_url} ./${PACKAGE_DIR}
+        ret_cp=$?
+        if [[ $ret_cp -eq 0 ]]; then
             echo "$(tput -T xterm setaf 2)download image ${image_package_name} ${image_package_version} package success$(tput -T xterm sgr0)"
             break
         fi
@@ -42,9 +45,53 @@ download_images_package() {
 
 }
 
+check_oss_tool() {
+    OSSUTIL_CLI="$( command -v ossutil )"
+    if [[ -n "$OSSUTIL_CLI" ]]; then
+        return
+    fi
+    echo "Install ossutil..."
+    for i in {1..3}; do
+        curl -fsSL https://gosspublic.alicdn.com/ossutil/install.sh | sudo bash
+        ret_install=$?
+        if [[ $ret_install -eq 0 ]]; then
+            echo "$(tput -T xterm setaf 2)Install ossutil success$(tput -T xterm sgr0)"
+            break
+        fi
+        sleep 1
+    done
+
+}
+
+oss_config() {
+    check_oss_tool
+    for i in {1..3}; do
+        ossutil config --endpoint=${OSS_ENDPOINT} --access-key-id=${OSS_ACCESS_KEY_ID} --access-key-secret=${OSS_ACCESS_KEY_SECRET}
+        ret_config=$?
+        if [[ $ret_config -eq 0 ]]; then
+            echo "$(tput -T xterm setaf 2)Config ossutil success$(tput -T xterm sgr0)"
+            break
+        fi
+        sleep 1
+    done
+}
+
 main() {
+    if [[ -z "${OSS_ACCESS_KEY_ID}" || -z "${OSS_ACCESS_KEY_SECRET}" ]]; then
+        echo "$(tput -T xterm setaf 1)Please provide oss access credential!$(tput -T xterm sgr0)"
+        return
+    fi
+
     if [[ ! -f "${MANIFESTS_FILE}" || ! -f "${VALUES_FILE}" ]]; then
         echo "$(tput -T xterm setaf 1)Not found manifests file:${MANIFESTS_FILE}$(tput -T xterm sgr0)"
+        return
+    fi
+
+    oss_config
+
+    OSSUTIL_CLI="$( command -v ossutil )"
+    if [[ -z "$OSSUTIL_CLI" ]]; then
+        echo "$(tput -T xterm setaf 1)Not found ossutil tools$(tput -T xterm sgr0)"
         return
     fi
 
@@ -69,7 +116,7 @@ main() {
             chart_name="kubeblocks-enterprise"
         fi
 
-        if [[ "${chart_enable}" == "true" && -n "${chart_version}" && "${is_addon}" == "true" ]]; then
+        if [[ "${chart_enable}" == "true" && -n "${chart_version}" && ("${is_addon}" == "true" || "${chart_name}" == "kubeblocks-enterprise") ]]; then
             if [[ "${PARALLEL_DOWNLOAD}" == "true" ]]; then
                 while [[ $(cat "${PARALLEL_FILE}") -ge ${MAX_PARALLEL_NUM} ]]; do
                     sleep 1
